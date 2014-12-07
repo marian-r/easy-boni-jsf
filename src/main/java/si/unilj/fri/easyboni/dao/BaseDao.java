48425b1b9486c27df8e0ec0575d80f24c9836bec
Marian Rusnak <grizly21@gmail.com>
2014-12-07 19:01:08 +0100

    Heroku postgres support

--- src/main/java/si/unilj/fri/easyboni/dao/BaseDao.java
+++ src/main/java/si/unilj/fri/easyboni/dao/BaseDao.java
@@ -11,7 +11,29 @@ public abstract class BaseDao {
     protected EntityManager em;
 
     public BaseDao() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("EasyBoniPU");
+        EntityManagerFactory emf;
+        String databaseUrl = System.getenv("DATABASE_URL");
+
+        if (databaseUrl != null) {
+            StringTokenizer st = new StringTokenizer(databaseUrl, ":@/");
+            String dbVendor = st.nextToken(); //if DATABASE_URL is set
+            String userName = st.nextToken();
+            String password = st.nextToken();
+            String host = st.nextToken();
+            String port = st.nextToken();
+            String databaseName = st.nextToken();
+            String jdbcUrl = String.format("jdbc:postgresql://%s:%s/%s?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", host, port, databaseName);
+            Map<String, String> properties = new HashMap<>();
+            properties.put("javax.persistence.jdbc.url", databaseUrl);
+            properties.put("javax.persistence.jdbc.user", userName);
+            properties.put("javax.persistence.jdbc.password", password);
+            properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+            properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            emf = Persistence.createEntityManagerFactory("EasyBoniPU", properties);
+        } else {
+            emf = Persistence.createEntityManagerFactory("EasyBoniPU");
+        }
+
         em = emf.createEntityManager();
     }
 }
